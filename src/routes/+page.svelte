dba315a69f38047738e916ec02bb90ad049d6f06
Gerrit Wanderer <gerrit.wanderer@gmail.com>
2024-10-18 10:58:30 +0200

    Add new headers and enable codelinting + prettier formatting in neovim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,6 @@
 	<title>{config.title}</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <section>
 	<ul class="posts">
 		{#each data.posts as post}
